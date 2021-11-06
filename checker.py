efc5934825f602bec9b229e9b33e19a83bf45214
VitolLub <VitolLub@gmail.com>
2021-11-06 08:39:36 +0200

    Add checker
    Class check every item have some changes
    If changes available  then change data by product ID

---
+++ checker.py
@@ -0,0 +1,9 @@
+__author__      = "Lubomir Vitol"
+__copyright__   = "Copyright 2021, Planet Earth"
+"""
+
+Class check every item have some changes
+If changes available  then change data by product ID
+Read data from aliexpress_all_product_ids
+
+"""
